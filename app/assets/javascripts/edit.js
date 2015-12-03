e296c945f8225b2da6758fdeab97d2eac5c32af7
Erik Germani <erikg885@gmail.com>
2015-12-03 01:53:32 -0600

    Drag spots, fix numeral positioning

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -1,15 +1,30 @@
 $( document ).ready(function () {
     var tool = new paper.Tool();
     tool.activate();
-    var spots = [];
+    spots = [];
     var texts = [];
     var spotCounter = 0;
 
-    raster = new Raster('game-image');
+    var hitOptions = {
+        fill: true,
+        tolerance: 5
+    };
+
+    var path;
+    var movePath = false;
+
+    var raster = new Raster('game-image');
     // var raster = new Raster('game-image');
 
     // Move the raster to the center of the view
     raster.position = view.center;
+
+    function deselect() {
+        var activated = project.getItems({selected: true})
+                for (var i = activated.length - 1; i >= 0; i--) {
+                    activated[i].selected = false;
+                };
+    }
 
     function Spot(spotSize, xCoord, yCoord, marker) {
         this.marker = marker
@@ -23,15 +38,15 @@ $( document ).ready(function () {
                 });
       this.path.fillColor.alpha = 0.0;
       this.path.data = marker;
-      this.path.onClick = function(event) {
-        // debugger;
-        this.remove();
-        spots[this.data-1] = null;
-        texts[this.data-1].remove();
-    }
+      // this.path.onClick = function(event) {
+      //   // debugger;
+      //   this.remove();
+      //   spots[this.data-1] = null;
+      //   texts[this.data-1].remove();
+    // }
       spots.push(this.path);
       this.text = new paper.PointText({
-        point: [xCoord - (spotSize * 2), yCoord - spotSize],
+        point: [xCoord - (spotSize + 15), yCoord - (spotSize + 5)],
         content: this.marker,
         fillColor: 'white',
         fontFamily: 'Courier New',
@@ -41,12 +56,39 @@ $( document ).ready(function () {
     };
 
     tool.onMouseDown = function (event) {
-        // debugger;
-        if (document.getElementById("mode_add").checked) {
+        path = null;
+        var hitResult = project.hitTest(event.point, hitOptions);
+
+        if (hitResult && hitResult.item.getClassName() != "Path") {
+            deselect();
             spotCounter++;
             new Spot(15, event.point.x, event.point.y, spotCounter);
+        }
+
+        if (hitResult && hitResult.item.getClassName() == "Path") {
+            path = hitResult.item;
+            movePath = hitResult.type == 'fill';
+            if (movePath) {
+                deselect();
+                hitResult.item.selected = true;
+                project.activeLayer.addChild(hitResult.item);
+                // debugger;
+            };
         };
+        // // OLD VERSION
+        // // debugger;
+        // if (document.getElementById("mode_add").checked) {
+        //     spotCounter++;
+        //     new Spot(15, event.point.x, event.point.y, spotCounter);
+        // };
     }
+
+    tool.onMouseDrag = function (event) {
+    if (path) {
+        path.position += event.delta;
+        texts[path.data-1].position += event.delta;
+    }
+}
 
     // For each spot associated with the image, create one, and push it into an array.
     var data = $('#myCanvas').data('spots');
