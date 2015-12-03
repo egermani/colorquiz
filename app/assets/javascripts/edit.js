f73d9cfdec4bfc06e252fabf5944fb365169fce1
Erik Germani <erikg885@gmail.com>
2015-12-03 02:54:08 -0600

    Implement spot scaling

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -1,7 +1,7 @@
 $( document ).ready(function () {
     var tool = new paper.Tool();
     tool.activate();
-    spots = [];
+    var spots = [];
     var texts = [];
     var spotCounter = 0;
 
@@ -19,10 +19,26 @@ $( document ).ready(function () {
     // Move the raster to the center of the view
     raster.position = view.center;
 
-    function deselect() {
-        var activated = project.getItems({selected: true})
+    function getActivated() {
+        var activated = project.getItems({
+            selected: true,
+            className: "Path"
+        });
+        return activated
+    }
+
+    function deselect(activated) {
+        var activated = getActivated()
                 for (var i = activated.length - 1; i >= 0; i--) {
                     activated[i].selected = false;
+                };
+    }
+
+    function deleteActive(activated) {
+        var activated = getActivated()
+        for (var i = activated.length - 1; i >= 0; i--) {
+                    activated[i].remove();
+                    texts[activated[i].data-1].remove();
                 };
     }
 
@@ -46,13 +62,14 @@ $( document ).ready(function () {
     // }
       spots.push(this.path);
       this.text = new paper.PointText({
-        point: [xCoord - (spotSize + 15), yCoord - (spotSize + 5)],
+        point: [this.path.bounds.topLeft.x - 15, this.path.bounds.topLeft.y - 3],
         content: this.marker,
         fillColor: 'white',
         fontFamily: 'Courier New',
         fontSize: 20
         });
       texts.push(this.text);
+      return this.path
     };
 
     tool.onMouseDown = function (event) {
@@ -62,7 +79,8 @@ $( document ).ready(function () {
         if (hitResult && hitResult.item.getClassName() != "Path") {
             deselect();
             spotCounter++;
-            new Spot(15, event.point.x, event.point.y, spotCounter);
+            var newSpot = new Spot(15, event.point.x, event.point.y, spotCounter);
+            newSpot.selected = true;
         }
 
         if (hitResult && hitResult.item.getClassName() == "Path") {
@@ -87,8 +105,41 @@ $( document ).ready(function () {
     if (path) {
         path.position += event.delta;
         texts[path.data-1].position += event.delta;
+        }
     }
-}
+
+    tool.onKeyDown = function (event) {
+        if (event.key == 'backspace') {
+        // Scale the path by 110%:
+        deleteActive();
+
+        // Prevent the key event from bubbling
+        return false;
+        } else if (event.key == 'right') {
+        // Scale the path by 110%:
+        path = getActivated()[0];
+        path.scale(1.1);
+        // debugger;
+        // texts[path.data-1].position = path.bounds.topLeft
+        // texts[path.data-1].position += new Point(-10, -10)
+        texts[path.data-1].position = new Point(path.bounds.topLeft.x - 10, path.bounds.topLeft.y - 10)
+
+
+        // Prevent the key event from bubbling
+        return false;
+        } else if (event.key == 'left') {
+        // Scale the path by 110%:
+        path = getActivated()[0];
+        path.scale(0.9);
+        // debugger;
+        // texts[path.data-1].position = path.bounds.topLeft
+        // texts[path.data-1].position += new Point(-10, -10)
+        texts[path.data-1].position = new Point(path.bounds.topLeft.x - 10, path.bounds.topLeft.y - 10)
+
+        // Prevent the key event from bubbling
+        return false;
+        }
+    }
 
     // For each spot associated with the image, create one, and push it into an array.
     var data = $('#myCanvas').data('spots');
