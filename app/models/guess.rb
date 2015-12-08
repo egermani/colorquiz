dedb83a00d2f4b37fa00ce021d206bdf119f858d
Erik Germani <erikg885@gmail.com>
2015-12-07 21:32:32 -0600

    Render game image on results page, track guess delta in column

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,7 +1,11 @@
 class Guess < ActiveRecord::Base
-  include Colorable
-  
+  include Colorable  
   belongs_to :round
   belongs_to :spot
   belongs_to :guesser, :class_name => "User"
+  before_save :score
+
+  def score
+    self.delta ||= Colorable::delta(self, spot)
+  end
 end
