863ebad663a232599ca5d2fdb2cb270cc08d1483
Erik Germani <erikg885@gmail.com>
2015-12-26 08:50:17 -0600

    Create D3 viz for guess accuracy over time

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -3,4 +3,17 @@ class GuessesController < ApplicationController
     # @guesses = Guess.all.sort_by {|guess| guess.spot.lab[0]}
     @guesses = Guess.all.sort_by {|guess| guess.delta}
   end
+
+  def stats
+    # Rails.logger.info(request.env)
+    respond_to do |format|
+      format.html {  }
+    end
+  end
+
+  def data
+    respond_to do |format|
+      format.json { render :json => Guess.all.order(created_at: :asc), :only => [:created_at, :delta] }
+    end
+  end
 end
