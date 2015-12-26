476a46f10d190ba48f93ddbdb7ffc60c5768cd02
Erik Germani <erikg885@gmail.com>
2015-12-26 10:07:26 -0600

    Figure out d3 JSON requests

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -8,12 +8,7 @@ class GuessesController < ApplicationController
     # Rails.logger.info(request.env)
     respond_to do |format|
       format.html {  }
-    end
-  end
-
-  def data
-    respond_to do |format|
-      format.json { render :json => Guess.all.order(created_at: :asc), :only => [:created_at, :delta] }
+      format.json { render :json => Guess.all.order(created_at: :asc), :only => [:created_at, :delta] } 
     end
   end
 end
