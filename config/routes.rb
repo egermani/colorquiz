dfeb22ebf7baa9b4fa7293cf4b6eba8eb602fcf2
Erik Germani <erikg885@gmail.com>
2016-01-06 07:24:22 -0600

    Add welcome text to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'rounds/create'
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
