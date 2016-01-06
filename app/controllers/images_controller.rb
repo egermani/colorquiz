e6d74c4e0ffea5018b08bcefea08bbdb345db4d9
Erik Germani <erikg885@gmail.com>
2016-01-06 08:51:39 -0600

    Implement cancan authorization and basic guest function

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,6 @@
 class ImagesController < ApplicationController
   before_action :set_image, only: [:show, :edit, :update, :destroy, :play]
+  load_and_authorize_resource
 
   def index
     @images = Image.all
