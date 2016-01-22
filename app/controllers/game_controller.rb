a5e6ea67dc6b7e76e6229d945c12e911907beca3
Erik Germani <erikg885@gmail.com>
2016-01-21 19:18:34 -0600

    Add quizround model

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -3,6 +3,6 @@ class GameController < ApplicationController
   def splash
     @user = User.new
     # render :layout => false
-    redirect_to play_quiz_path(Quiz.find_by(name: "The Beta Quiz"))
+    redirect_to quiz_path(Quiz.find_by(name: "The Beta Quiz"))
   end
 end
